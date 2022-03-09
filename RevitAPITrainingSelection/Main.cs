41c44c10f858052b1e17adfd842a264dd02d6136
Татьяна <veselovskayt@mail.ru>
2022-03-09 19:26:45 +0700

    Добавьте файлы проекта.

---
+++ RevitAPITrainingSelection/Main.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSelection
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Edge, new WallFilter(), "Выберите стены");
+            var wallList = new List<Wall>();
+
+            string info = string.Empty;
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Wall oWall = doc.GetElement(selectedElement) as Wall;
+                wallList.Add(oWall);
+                var width = UnitUtils.ConvertFromInternalUnits(oWall.Width, UnitTypeId.Millimeters);
+                info += $"Name: {oWall.Name}, width: {width} {Environment.NewLine}";
+            }
+
+            info += $"Количество: {wallList.Count}";
+
+            TaskDialog.Show("Selection", info);
+
+            return Result.Succeeded;
+        }
+    }
+}
